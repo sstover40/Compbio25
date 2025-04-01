c6fa0e4ddc9b70c34980b789b262ea1146214e81
sstover40 <sstover@uvm.edu>
2025-04-01 14:45:33 -0400

    notes

--- ggplot2.R
+++ ggplot2.R
@@ -127,13 +127,24 @@ p1 + theme_grey() # ggplots default theme
 p1 + theme_classic(base_size=40,base_family="serif")
 # defaults: theme_grey, base_size=16,base_family="Helvetica")
 # font families (Mac): Times, Ariel, Monaco, Courier, Helvetica, serif,sans
+# defaults: theme_grey, base_size=16,base_family="Helvetica")
+# font families (Mac): Times, Ariel, Monaco, Courier, Helvetica, serif,sans
+
+
+
+# code for adding additional fonts, can look in R studio to see list of fonts
+library(extrafont)
+font_import()  # Imports all system fonts (run once)
+# loadfonts(device = "win")  # For Windows
+# fonts() # will list loaded fonts
+p1 + theme_classic(base_size=35,
+                   base_family="Chalkduster")
 
 # use coordinate_flip to invert entire plot
 p2 <- ggplot(data=d, mapping=aes(x=fl,fill=fl)) + geom_bar()
 print(p2)
-p2 + coord_flip() + theme_grey(base_size=20,base_family="sans")
-
-geom_point(size=7,shape=21,color="black",fill="steelblue") +
+p2 + coord_flip() + theme_grey(base_size=20,base_family="sans") +
+  geom_point(size=7,shape=21,color="black",fill="steelblue") +
   labs(title="My graph title here",
        subtitle="An extended subtitle that will print below the main title",
        x="My x axis label",
@@ -141,9 +152,205 @@ geom_point(size=7,shape=21,color="black",fill="steelblue") +
   xlim(0,4) + ylim(0,20)
 print(p1)
 
+# use labs for different plot labels
+p1 <- ggplot(data=d) +
+  aes(x=displ,y=cty) +
+  geom_point() +
+  labs(title="My graph title here",
+       subtitle="An extended subtitle",
+       x="Displacement",
+       y="City Mileage",
+       caption="Add a caption here") +
+  theme_bw(base_size=25,base_family="Monaco")
+#    xlim(0,4) + ylim(0,20)
+print(p1)
+
+# use attributes for point size shape, color
+p1 <- ggplot(data=d) +
+  aes(x=displ,y=cty) +
+  geom_point(size=4,
+             shape=21,
+             color="black",fill="cyan") +
+  theme_bw(base_size=25,base_family="Monaco")
+print(p1)
+
+# use x and/or y limits to clip data set
+p1 <- ggplot(data=d) +
+  aes(x=displ,y=cty) +
+  geom_point(size=4,
+             shape=21,
+             color="black",fill="cyan") +
+  xlim(4,7) +
+  ylim(-10,40) +
+  theme_bw(base_size=25,base_family="Monaco")
+print(p1)
+
+####----------------------------------------------------------------------------
+
+library(ggplot2)
+library(ggthemes)
+library(patchwork)
+d <- mpg # use built in mpg data frame
+g1 <- ggplot(data=d) +
+  aes(x=displ,y=cty) +
+  geom_point() +
+  geom_smooth()
+print(g1)
+
+g2 <- ggplot(data=d) +
+  aes(x=fl) +
+  geom_bar(fill="tomato",color="black")+
+  theme(legend.position="none")
+print(g2)
+
+g3 <- ggplot(data=d) +
+  aes(x=displ) +
+  geom_histogram(fill="royalblue",
+                 color="black")
+print(g3)
+
+g4 <- ggplot(data=d) +
+  aes(x=fl,y=cty,fill=fl) +
+  geom_boxplot() +
+  theme(legend.position="none")
+
+print(g4)
+
+# place two plots horizontally
+g1 + g2
+
+# place 3 plots vertically
+g1 + g2 + g3 + plot_layout(ncol=1)
+# change relative area of each plot
+g1 + g2 + plot_layout(ncol=1,heights=c(2,1))
+
+g1 + g2 + plot_layout(ncol=2,widths=c(1,2))
+# add a spacer plot
+g1 + plot_spacer() + g2
+# use nested layouts
+g1 + {
+  g2 + {
+    g3 +
+      g4 +
+      plot_layout(ncol=1)
+  }
+} +
+  plot_layout(ncol=1)
+
+# - operator for subtrack placement
+g1 + g2 - g3 + plot_layout(ncol=1)
+# / and | for intuitive layouts
+(g1 | g2 | g3)/g4
+(g1 | g2)/(g3 | g4)
+# Add title, etc. to a patchwork
+g1 + g2 + plot_annotation('This is a title',
+                          caption = 'made with patchwork')
+
+# Change styling of patchwork elements
+g1 + g2 +
+  plot_annotation(
+    title = 'This is a title',
+    caption = 'made with patchwork',
+    theme = theme(plot.title = element_text(size = 16))
+  )
+
+# Add tags to plots
+g1 / (g2 | g3) +
+  plot_annotation(tag_levels = 'A')
+
+#Swapping axis orientation for a multipanel plot
+g3a <- g3 + scale_x_reverse()
+g3b <- g3 + scale_y_reverse()
+g3c <- g3 + scale_x_reverse() + scale_y_reverse()
+
+(g3 | g3a)/(g3b | g3c)
+
+(g3 + coord_flip() | g3a + coord_flip())/(g3b + coord_flip() | g3c + coord_flip())
+
+#REMEMBER: need to save the plots once you are done with them!!!
+ggsave(filename="MyPlot.pdf",
+       plot=g3,
+       device="pdf",
+       width=20,
+       height=20,
+       units="cm",
+       dpi=300)
+
+# mapping of a discrete variable to point color
+m1 <- ggplot(data=mpg) +
+  aes(x=displ,y=cty,color=class) +
+  geom_point(size=3)
+print(m1)
+
+# mapping of a discrete variable to point shape (<= 6)
+m1 <- ggplot(data=mpg) +
+  aes(x=displ,y=cty,shape=class) +
+  geom_point(size=3)
+print(m1)
+
+# mapping of a discrete variable to point size (not advised)
+m1 <- ggplot(data=mpg) +                                   aes(x=displ,y=cty,size=class) +
+  geom_point()
+print(m1)
 
 
+#MORE USEFUL WAY TO VISUALIZE --> THIS IS A BUBBLE PLOT, can use it on a physical map
+#This is the plot that George suggested for Ericka's data
+# mapping a continuous variable to point size
+m1 <- ggplot(data=mpg) +
+  aes(x=displ,y=cty,size=hwy) +
+  geom_point()
+print(m1)
 
+# mapping a continuous variable to point color, HEAT MAP
+m1 <- ggplot(data=mpg) +
+  aes(x=displ,y=cty,color=hwy) +
+  geom_point(size=5)
+print(m1)
 
+# mapping two variables to different aesthetics, still using the miles per gallon data
+#change the shape of the points to represent the data, ggplot might not use shapes that
+#are easy to differentiate if there are more than 6 or so catagories
+m1 <- ggplot(data=mpg) + aes(x=displ,y=cty,shape=class,color=hwy) +
+  geom_point(size=5)
+print(m1)
+
+# use shape for smaller number of categories
+m1 <- ggplot(data=mpg) + aes(x=displ,y=cty,shape=drv,color=fl) +
+  geom_point(size=5)
+# use all 3 (size, shape, color) to indicate 5 attributes!
+m1 <- ggplot(data=mpg) +
+  aes(x=displ,
+      y=cty,shape=drv,
+      color=fl,
+      size=hwy) +
+  geom_point()
+print(m1)
+
+# mapping a variable to the same aesthetic in two different geoms
+m1 <- ggplot(data=mpg) +
+  aes(x=displ,y=cty,color=drv) +
+  geom_point(size=2) +
+  geom_smooth(method="lm")a
+print(m1)
+
+#### FACETING
+#faceting variables for more effective grouping
+# basic faceting with variables split by row, column, or both
+m1 <- ggplot(data=mpg) +
+  aes(x=displ,y=cty) +
+  geom_point()
+
+#these will all give you different combinations
+m1 +  facet_grid(class~fl) #which graph and which fuel type
+m1 + facet_grid(class~fl, scales="free_y")
+m1 + facet_grid(class~fl, scales="free")
+m1 + facet_grid(.~class)
+m1 + facet_grid(class~.)
+# use facet wrap when variables are not crossed
+m1 + facet_grid(.~class)
+m1 +  facet_wrap(~class)
+m1 + facet_wrap(~class + fl)
+m1 + facet_wrap(~class + fl, drop=FALSE)
 
 
