5f50801c854a2de8263817a70a64a42f91f3c0ac
sstover40 <sstover@uvm.edu>
2025-04-02 22:25:37 -0400

    HW

--- Homework 9 Project/Functions/DataClean.R
+++ Homework 9 Project/Functions/DataClean.R
@@ -1,23 +1,29 @@
 # --------------------------------------
 # FUNCTION data_clean
 # required packages: none
-# description:
-# inputs:
-# outputs:
+# description: this function goes into a list of .csv files and gets rid of any columns that have NAs
+#if there are some columns with one or 2 NAs then you need to change the settings
+# inputs: a data frame .csv file
+# outputs: the same data frame but minus rows with NAs
 ########################################
-data_clean <- function(x=NULL,y=NULL){
+data_clean <- function(x=NULL){
 
 # assign parameter defaults
-if (is.null(x) | is.null(y)) {
-  x <- runif(10)
-  y <- runif(10)
+if (is.null(x)) {
+  print("your data frame does not exist! try checking the path or file name")
 }
 
-# function body
+# function body: need to get rid of rows with NAs in 2 catagories:
+  # of species
+  # of birds
+#based on the tables cannot just get rid of rows that have NAs bc there is a
+#column that is majority NAs
+x <- data %>%
+  #only removing rows with NAs in count and scientific name/species
+  filter(!is.na(x$pointCountMinute) & !is.na(x$scientificName))
 
-
-
-return(print('...checking function: data_clean()'))
+return(print('...checking function: data_clean(x)'))
+return(x)
 
 } # end of function data_clean
 # --------------------------------------
---
+++ Homework 9 Project/Functions/FitRegression.R
@@ -0,0 +1,24 @@
+# --------------------------------------
+# FUNCTION fit_regression
+# required packages: none
+# description:
+# inputs:
+# outputs:
+########################################
+fit_regression <- function(x=NULL,y=NULL){
+
+# assign parameter defaults
+if (is.null(x) | is.null(y)) {
+  x <- runif(10)
+  y <- runif(10)
+}
+
+# function body
+
+
+
+return(print('...checking function: fit_regression()'))
+
+} # end of function fit_regression
+# --------------------------------------
+# fit_regression()
